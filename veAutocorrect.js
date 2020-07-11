9c72a854d0d3376e5b569af505939ddb2416adc8
Eccenux <eccenux+git@gmail.com>
2020-07-11 16:49:33 +0200

    2.0: refactor, custom commands

--- veAutocorrect.js
+++ veAutocorrect.js
@@ -1,14 +1,13 @@
 /**
  * Autocorrection features in Visual Editor.
  * 
- * Original documentation:
- * https://de.wikipedia.org/wiki/User:Schnark/js/veAutocorrect
- * 
- * English documentation:
- * https://en.wikipedia.org/wiki/User:Nux/veAutocorrect
- * 
  * Polska dokumentacja:
- * https://pl.wikipedia.org/wiki/User:Nux/veAutocorrect
+ *		https://pl.wikipedia.org/wiki/User:Nux/veAutocorrect
+ * English documentation:
+ *		https://en.wikipedia.org/wiki/User:Nux/veAutocorrect
+ * 
+ * Original author by Schnark.
+ *		https://de.wikipedia.org/wiki/User:Schnark/js/veAutocorrect
  * 
  * <nowiki>
  */
@@ -16,53 +15,105 @@
 (function (mw) {
 "use strict";
 
-function initAutoCorrect (lang, wiki) {
-
-	//Command to replace selected content and place the cursor after it
-	//inherit from ve.ui.Command, and override execute
-	function AutoCorrectCommand (name, content) {
-		AutoCorrectCommand.parent.call(this, name);
-		this.content = content;
-	}
-	OO.inheritClass(AutoCorrectCommand, ve.ui.Command);
-	AutoCorrectCommand.prototype.execute = function (surface) {
-		surface.getModel().getFragment().insertContent(this.content).collapseToEnd().select();
-		return true;
-	};
-
-	//like ve.ui.Sequence, with the difference that for regular expressions
-	//of the form /foo(bar)/ only the parentheses is used as Range, not the whole expression
-	function ReSequence () {
-		ReSequence.parent.apply(this, arguments);
-	}
-	OO.inheritClass(ReSequence, ve.ui.Sequence);
-	ReSequence.prototype.match = function (data, offset, plaintext) {
-		var execResult;
-		if (this.data instanceof RegExp) {
-			execResult = this.data.exec(plaintext);
-			return execResult && new ve.Range(offset - execResult[1].length, offset);
+/**
+ * Autocorrect class (export).
+ */
+window.veNuxAutocorrect = {
+	version: 2.0,
+	
+	_ready: false,
+	_configs: [],
+	
+	/**
+	 * Add replacemnt rule.
+	 *
+	 * Examples in documentation.
+	 * See also: `autoCorrectFromTo`.
+	 */
+	addReplacements: function(config) {
+		if (this._ready) {
+			this._run(config);
+		} else {
+			this._configs.push(config);
 		}
-		return ReSequence.parent.prototype.match.apply(this, arguments);
-	};
+	},
+	
+	_run: function(config) {
+		autoCorrectFromTo(config.from, config.to);
+	},
+	
+	_onReady: function() {
+		for (var i = 0; i < this._configs.length; i++) {
+			this._run(this._configs[i]);
+		}
+		this._configs = [];
+		this._ready = true;
+	},
+};
 
-	var autoCorrectCommandCount = 0;
-	//when the user enters "from" change it to "to"
-	//from can be a string, a regular expression of the form /foo(bar)/ or an array of data
-	//to can be a string or an array of data
-	function autoCorrectFromTo (from, to) {
-		//get a unique name, we use it for both the command and the sequnce
-		var name = 'schnarkAutoCorrectCommand-' + (autoCorrectCommandCount++);
-		//create and register the command
-		ve.ui.commandRegistry.register(
-			new AutoCorrectCommand(name, to)
-		);
-		//let the surface know that there is a new command that can be executed
-		ve.init.target.getSurface().commands.push(name);
-		//create and register the sequence
-		ve.ui.sequenceRegistry.register(
-			new ReSequence(/*sequence*/ name, /*command*/ name, from, 0, true)
-		);
+/**
+ * AutoCorrectCommand.
+ * 
+ * Command to replace selected content and place the cursor after it.
+ * 
+ * inherit from ve.ui.Command, and override execute
+ */
+function AutoCorrectCommand (name, content) {
+	AutoCorrectCommand.parent.call(this, name);
+	this.content = content;
+}
+OO.inheritClass(AutoCorrectCommand, ve.ui.Command);
+AutoCorrectCommand.prototype.execute = function (surface) {
+	surface.getModel().getFragment().insertContent(this.content).collapseToEnd().select();
+	return true;
+};
+
+/**
+ * ReSequence.
+ * 
+ * like ve.ui.Sequence, with the difference that for regular expressions
+ * of the form /foo(bar)/ only the parentheses is used as Range, not the whole expression
+ */
+function ReSequence () {
+	ReSequence.parent.apply(this, arguments);
+}
+OO.inheritClass(ReSequence, ve.ui.Sequence);
+ReSequence.prototype.match = function (data, offset, plaintext) {
+	var execResult;
+	if (this.data instanceof RegExp) {
+		execResult = this.data.exec(plaintext);
+		return execResult && new ve.Range(offset - execResult[1].length, offset);
 	}
+	return ReSequence.parent.prototype.match.apply(this, arguments);
+};
+
+var autoCorrectCommandCount = 0;
+/**
+ * autoCorrectFromTo.
+ * 
+ * when the user enters "from" change it to "to"
+ * @param from can be a string, a regular expression of the form /foo(bar)/ or an array of data
+ * @param to can be a string or an array of data
+ */
+function autoCorrectFromTo (from, to) {
+	//get a unique name, we use it for both the command and the sequnce
+	var name = 'nuxAutoCorrectCommand-' + (autoCorrectCommandCount++);
+	//create and register the command
+	ve.ui.commandRegistry.register(
+		new AutoCorrectCommand(name, to)
+	);
+	//let the surface know that there is a new command that can be executed
+	ve.init.target.getSurface().commands.push(name);
+	//create and register the sequence
+	ve.ui.sequenceRegistry.register(
+		new ReSequence(/*sequence*/ name, /*command*/ name, from, 0, true)
+	);
+}
+
+/**
+ * Init commands.
+ */
+function initAutoCorrect (lang, wiki) {
 
 	//define what should be autocorrected
 
@@ -142,15 +193,8 @@ function initAutoCorrect (lang, wiki) {
 		break;
 	}
 	
-	// test
-	window.autoCorrectFromTo = autoCorrectFromTo;
-	
-	// custom
-	if (typeof veAutocorrectCustomRules === 'object' && Array.isArray(veAutocorrectCustomRules)) {
-		for (var i = 0; i < veAutocorrectCustomRules.length; i++) {
-			autoCorrectFromTo(veAutocorrectCustomRules[i].from, veAutocorrectCustomRules[i].to);
-		}
-	}
+	// run custom commands
+	veNuxAutocorrect._onReady();
 }
 
 //we just need to run once the editor is ready
