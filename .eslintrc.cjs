257c07f5d5a6ba36330443b2ac62a8f7659a0b72
Nux <eccenux+git@gmail.com>
2023-06-22 00:27:25 +0200

    eslint cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,17 @@
 module.exports = {
     "env": {
+		"node": true,
         "browser": true,
         "es2020": true
     },
+	"globals": {
+		"$": true,
+		"mw": true,
+	},
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": 11
+		"ecmaVersion": "latest",
+		"sourceType": "module"
     },
     "rules": {
 		"no-prototype-builtins": "off",
